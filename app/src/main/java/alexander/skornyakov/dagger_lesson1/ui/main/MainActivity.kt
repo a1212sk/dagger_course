1da7ea8bbe2cf2d395883e1e06b7002db8460221
Alexander Skornyakov <a1212sk@gmail.com>
2020-01-13 21:29:15 +0300

    add navigation drawer

--- app/src/main/java/alexander/skornyakov/dagger_lesson1/ui/main/MainActivity.kt
+++ app/src/main/java/alexander/skornyakov/dagger_lesson1/ui/main/MainActivity.kt
@@ -5,15 +5,25 @@ import alexander.skornyakov.dagger_lesson1.R
 import alexander.skornyakov.dagger_lesson1.ui.main.posts.PostsFragment
 import alexander.skornyakov.dagger_lesson1.ui.main.profile.ProfileFragment
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity(){
+class MainActivity : BaseActivity() , NavigationView.OnNavigationItemSelectedListener{
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
         testFragment()
     }
 
@@ -38,5 +48,19 @@ class MainActivity : BaseActivity(){
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container,PostsFragment())
             .commit()
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.nav_profile->{
+
+            }
+            R.id.nav_posts->{
+
+            }
+        }
+        p0.isChecked = true
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
